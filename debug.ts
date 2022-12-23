8bf8e6cf7c380a8861d2ed735d3d525b04df6628
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-12-23 12:21:36 +0700

    Fix operator fees

--- debug.ts
+++ debug.ts
@@ -1,4 +1,12 @@
+import { getBooleanEnvVar } from './process'
+
 export function passlog<Data>(label: string, data: Data) {
   console.log(label, data)
   return data
+}
+
+const isEnabledLog = getBooleanEnvVar('LOG', process.env.LOG)
+
+export function show<Args extends unknown[]>(...args: Args) {
+  if (isEnabledLog) console.info(...args)
 }
