da8ef16a0563d982ae2b515075f440cb25ce606c
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-01-12 21:15:03 +0700

    Refactor TransactionForm

--- zod/types.ts
+++ zod/types.ts
@@ -1,6 +1,9 @@
 import { ZodObject } from 'zod'
-import { objectInputType, objectOutputType, ZodRawShape, ZodTypeAny } from 'zod/lib/types'
+import { objectInputType, objectOutputType, SafeParseReturnType, ZodRawShape, ZodTypeAny } from 'zod/lib/types'
+import { ParseParams } from 'zod/lib/helpers/parseUtil'
 
 export type UnknownKeysParam = 'passthrough' | 'strict' | 'strip'
 
 export type ZodObjectStd<T extends ZodRawShape, UnknownKeys extends UnknownKeysParam = 'strip', Catchall extends ZodTypeAny = ZodTypeAny> = ZodObject<T, UnknownKeys, Catchall, objectOutputType<T, Catchall>, objectInputType<T, Catchall>>
+
+export type SafeParse<Input, Output> = (data: unknown, params?: Partial<ParseParams>) => SafeParseReturnType<Input, Output>
