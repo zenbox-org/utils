324efe7aaffb9559724b33e69cbd9a75e65dc08f
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-02-06 13:38:25 +0700

    Implement Canto deployments

--- conversion.ts
+++ conversion.ts
@@ -3,3 +3,5 @@ export const string2boolean = (value: string) => ['true', 'yes', 't', 'y', '1'].
 export const boolean2string = (value: boolean) => value ? 'true' : 'false'
 
 export const boolean2number = (value: unknown) => value ? 1 : 0
+
+export const string2array = (value: string) => value.split(',')
--- process.ts
+++ process.ts
@@ -1,27 +1,27 @@
-import { string2boolean } from './conversion'
+import { string2array, string2boolean } from './conversion'
+import { Mapper } from '../generic/models/Mapper'
+import { identity } from 'remeda'
+import { NonEmptyArray } from './array/types'
 
 /**
  * Next.js inlines `process.env.VAR_NAME` during build, so they must be passed directly
  */
-export function getEnvVar(name: string, value: string | undefined, $default?: string) {
-  const $value = value || $default
-  if ($value) return $value
-  throw new Error(`process.env.${name} is required. If you're running Next.js code on the client, prefix the var with "NEXT_PUBLIC_" to make it available on the frontend.`)
+export const getEnvVar = <T>(mapper: Mapper<string, T>) => ($name: string, $value: string | undefined, $default?: T): T => {
+  if ($value) return mapper($value)
+  if ($default) return $default
+  throw new Error(`process.env.${$name} is required. If you're running Next.js code on the client, prefix the var with "NEXT_PUBLIC_" to make it available on the frontend.`)
 }
 
-export function getArrayEnvVar(name: string, value: string | undefined, $default?: string): string[] {
-  const valuesAsString = getEnvVar(name, value, $default)
-  return valuesAsString.split(',')
-}
+export const getStringEnvVar = getEnvVar(identity)
 
-export function getNonEmptyArrayEnvVar(name: string, value: string | undefined, $default?: string): [string, ...string[]] {
+export const getBooleanEnvVar = getEnvVar(string2boolean)
+
+export const getArrayEnvVar = getEnvVar(string2array)
+
+export const getNonEmptyArrayEnvVar = (name: string, value: string | undefined, $default?: string[]): NonEmptyArray<string> => {
   const values = getArrayEnvVar(name, value, $default)
   if (!values.length) throw new Error(`process.env.${name} must be a comma-separated list with at least one element`)
   const first = values[0]
   const rest = values.slice(1)
   return [first, ...rest]
-}
-
-export function getBooleanEnvVar(name: string, value: string | undefined) {
-  return string2boolean(value || '')
 }
