043f389ca45c517fd07168f0694839ed4a36e492
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-02-25 16:05:15 +0700

    Fairpool

--- numeral/sort.ts
+++ numeral/sort.ts
@@ -1,3 +1,6 @@
+import { Mapper } from '../../generic/models/Mapper'
 import { Numeral } from './types'
 
 export const compareNumerals = <T extends Numeral>(a: T, b: T) => (a < b) ? -1 : ((a > b) ? 1 : 0)
+
+export const compareNumeralsBy = <T, Num extends Numeral>(mapper: Mapper<T, Num>) => (a: T, b: T) => compareNumerals(mapper(a), mapper(b))
