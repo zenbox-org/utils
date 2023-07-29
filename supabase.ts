95516f163a2a2bf759dfa780e927ea9f3338751a
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-07-29 16:49:34 +0700

    Upgrade Supabase

--- supabase.ts
+++ supabase.ts
@@ -1,24 +1,38 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createClient, SupabaseClient, SupabaseClientOptions } from '@supabase/supabase-js'
+import { GenericSchema } from '@supabase/supabase-js/src/lib/types'
 
 let supabasePublic: SupabaseClient
 let supabasePrivate: SupabaseClient
 
-export function getSupabasePublic() {
+export const getSupabasePublic = () => {
   if (!process.env.NEXT_PUBLIC_SUPABASE_URL) throw new Error('process.env.NEXT_PUBLIC_SUPABASE_URL is required')
   if (!process.env.NEXT_PUBLIC_SUPABASE_KEY) throw new Error('process.env.NEXT_PUBLIC_SUPABASE_KEY is required')
   return supabasePublic || (supabasePublic = createSupabase(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY))
 }
 
-export function getSupabasePrivate() {
+export const getSupabasePrivate = () => {
   if (!process.env.NEXT_PUBLIC_SUPABASE_URL) throw new Error('process.env.NEXT_PUBLIC_SUPABASE_URL is required')
   if (!process.env.NEXT_PRIVATE_SUPABASE_KEY) throw new Error('process.env.NEXT_PRIVATE_SUPABASE_KEY is required')
-  return supabasePrivate || (supabasePrivate = createSupabase(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PRIVATE_SUPABASE_KEY))
+  return supabasePrivate || (supabasePrivate = createSupabase(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PRIVATE_SUPABASE_KEY, {
+    auth: {
+      persistSession: false,
+    },
+  }))
 }
 
-export function createSupabase(supabaseUrl: string, supabaseKey: string) {
+export const createSupabase = <
+  Database = any,
+  SchemaName extends string & keyof Database = 'public' extends keyof Database
+    ? 'public'
+    : string & keyof Database,
+  Schema extends GenericSchema = Database[SchemaName] extends GenericSchema
+    ? Database[SchemaName]
+    : any
+>(supabaseUrl: string, supabaseKey: string, options?: SupabaseClientOptions<SchemaName>): SupabaseClient<Database, SchemaName, Schema> => {
   if (!supabaseUrl) throw new Error('supabaseUrl is required')
   if (!supabaseKey) throw new Error('supabaseKey is required')
-  return createClient(supabaseUrl, supabaseKey)
+  return createClient(supabaseUrl, supabaseKey, options)
 }
 
 export async function sb<T>(supabaseResult: Promise<{ data: T | null; error: Error | null }>): Promise<T> {
