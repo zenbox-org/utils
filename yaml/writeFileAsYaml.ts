51aa8e9e1e80082540064293440c51eb96f9748d
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-07-29 09:01:38 +0700

    Fix build

--- yaml/writeFileAsYaml.ts
+++ yaml/writeFileAsYaml.ts
@@ -1,8 +1,8 @@
-import { stringify } from 'yaml'
-import { BaseEncodingOptions, Mode, OpenMode, PathLike } from 'fs'
+import { EncodingOption, Mode, OpenMode, PathLike } from 'fs'
 import { FileHandle, writeFile } from 'fs/promises'
+import { stringify } from 'yaml'
 
-export async function writeFileAsYaml(path: PathLike | FileHandle, obj: unknown, options?: BaseEncodingOptions & { mode?: Mode | undefined, flag?: OpenMode | undefined } | BufferEncoding | null) {
+export async function writeFileAsYaml(path: PathLike | FileHandle, obj: unknown, options?: EncodingOption & { mode?: Mode | undefined, flag?: OpenMode | undefined } | BufferEncoding | null) {
   const string = stringify(obj)
   return writeFile(path, string, options)
 }
