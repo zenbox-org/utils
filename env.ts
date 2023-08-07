ed4ec6f5f740ad9101d1a3bc66797af4145b96bd
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-08-07 12:26:56 +0700

    Upgrade packages

--- env.ts
+++ env.ts
@@ -1,27 +1,31 @@
-import { parse } from 'dotenv'
+import { DotenvParseOutput, parse } from 'dotenv'
 import { PathLike } from 'fs'
 import { readFile } from 'fs/promises'
 import { merge } from 'remeda'
+import * as process from 'process'
 import ProcessEnv = NodeJS.ProcessEnv
 
 export type Env = Record<string, string | undefined> & ProcessEnv
 
 /**
- * NOTE: This function updates process.env
+ * NOTE: This function modifies process.env
  */
-export async function loadEnv(): Promise<Env> {
+export async function loadEnv(): Promise<void> {
   const cwd = process.cwd()
-  const configs = await Promise.all([
-    getEnvFromFileMaybe(`${cwd}/.env`),
-    getEnvFromFileMaybe(`${cwd}/.env.development`),
-    getEnvFromFileMaybe(`${cwd}/.env.local`),
-  ])
-  return configs.reduce<ProcessEnv>(function (fullConfig, partialConfig) {
-    return merge(fullConfig, partialConfig)
-  }, process.env)
+  const config = await getDotenvParseOutput(cwd)
+  Object.assign(process.env, config)
 }
 
-export async function getEnvFromFileMaybe(filename: PathLike) {
+export async function getDotenvParseOutput(dir: string) {
+  const configs = await Promise.all([
+    getEnvFromFileMaybe(`${dir}/.env`),
+    getEnvFromFileMaybe(`${dir}/.env.development`),
+    getEnvFromFileMaybe(`${dir}/.env.local`),
+  ])
+  return configs.reduce((fullConfig, partialConfig) => merge(fullConfig, partialConfig), {})
+}
+
+export async function getEnvFromFileMaybe(filename: PathLike): Promise<DotenvParseOutput> {
   return readFile(filename).then(parse).catch((e) => {
     if (e.code === 'ENOENT') {
       return {}
