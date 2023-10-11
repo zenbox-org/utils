2fcffe4f6d5b9fcb9b9d3b482cbae5e1d65c977b
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-10-11 18:51:00 +0700

    aider: Implemented `add` function for Maps.
    # Aider chat conversation:
    
    USER: Implement `add`
    
    ASSISTANT: To implement the `add` function, we need to iterate over the keys of both maps. For each key, we will add the values from both maps using the `arithmetic.add` function. If a key is not present in one of the maps, we will use the value from the other map.
    
    Here is the step-by-step plan:
    
    1. Create a new Map `result`.
    2. Iterate over the keys of `a` and `b` using a `for...of` loop.
    3. For each key, check if it exists in both `a` and `b`.
    4. If it does, add the values from both maps using `arithmetic.add` and set the result in `result` for the current key.
    5. If the key exists only in one of the maps, set the value from that map in `result` for the current key.
    6. Return `result`.
    
    Here is the code change:
    
    ```typescript
    Map/add.ts
    <<<<<<< HEAD
    import { BasicArithmetic } from '../arithmetic'
    import { todo } from '../todo'
    
    export const add = <V>(arithmetic: BasicArithmetic<V>) => <K, V>(a: Map<K, V>, b: Map<K, V>) => {
      return todo<Map<K, V>>()
    }
    =======
    import { BasicArithmetic } from '../arithmetic'
    
    export const add = <V>(arithmetic: BasicArithmetic<V>) => <K, V>(a: Map<K, V>, b: Map<K, V>) => {
      const result = new Map<K, V>()
      const allKeys = new Set([...a.keys(), ...b.keys()])
      for (const key of allKeys) {
        const aValue = a.get(key)
        const bValue = b.get(key)
        if (aValue !== undefined && bValue !== undefined) {
          result.set(key, arithmetic.add(aValue, bValue))
        } else if (aValue !== undefined) {
          result.set(key, aValue)
        } else if (bValue !== undefined) {
          result.set(key, bValue)
        }
      }
      return result
    }
    >>>>>>> updated
    ```

--- Map/add.ts
+++ Map/add.ts
@@ -1,1 +1,18 @@
-// Map add
+import { BasicArithmetic } from '../arithmetic'
+
+export const add = <V>(arithmetic: BasicArithmetic<V>) => <K, V>(a: Map<K, V>, b: Map<K, V>) => {
+  const result = new Map<K, V>()
+  const allKeys = new Set([...a.keys(), ...b.keys()])
+  for (const key of allKeys) {
+    const aValue = a.get(key)
+    const bValue = b.get(key)
+    if (aValue !== undefined && bValue !== undefined) {
+      result.set(key, arithmetic.add(aValue, bValue))
+    } else if (aValue !== undefined) {
+      result.set(key, aValue)
+    } else if (bValue !== undefined) {
+      result.set(key, bValue)
+    }
+  }
+  return result
+}
