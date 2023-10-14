fce1ce1f5580afa16c719508d4ce6b6857af71f7
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-10-14 13:46:28 +0700

    Thinking

--- yargs.ts
+++ yargs.ts
@@ -1,5 +1,8 @@
 import { getHumanName } from './filesystem'
 
-export function getCommandName(filename: string) {
-  return getHumanName(filename)
+export function getCommandName(filename: string, args: string[] = []) {
+  const cmdName = getHumanName(filename)
+  const argNames = args.map(arg => `<${arg}>`).join(' ')
+  const names = [cmdName, ...argNames]
+  return names.join(' ')
 }
