65b1ce6169f7f3000456e76494ffec574f73b866
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-10-21 16:30:27 +0700

    Mapper

--- Validator/seq.ts
+++
@@ -1,9 +0,0 @@
-import { ProducerBatchStatic } from '../../decimaker/models/Producer'
-
-export const seq = <Err>(producers: ProducerBatchStatic<Err>[]): Err[] => {
-  for (const producer of producers) {
-    const errors = producer()
-    if (errors.length) return errors
-  }
-  return []
-}
